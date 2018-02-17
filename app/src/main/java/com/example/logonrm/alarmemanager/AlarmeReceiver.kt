4baac28aacdeaf38f6e96d697a34389dda471b84
logonrm <lagmat2002@yahoo.com.br>
2018-02-17 15:10:09 -0200

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/alarmemanager/AlarmeReceiver.kt
@@ -0,0 +1,21 @@
+package com.example.logonrm.alarmemanager
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.widget.Toast
+
+/**
+ * Created by logonrm on 17/02/2018.
+ */
+class AlarmeReceiver : BroadcastReceiver(){
+    private var mp: MediaPlayer? = null
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        mp = MediaPlayer.create(context, R.raw.alarm)
+        mp!!.start();
+        Toast.makeText(context, "Alarme....", Toast.LENGTH_LONG).show()
+    }
+
+}
---
+++ app/src/main/java/com/example/logonrm/alarmemanager/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.logonrm.alarmemanager
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bt_iniciar.setOnClickListener{
+            disparar()
+        }
+    }
+    fun disparar(){
+        val tempo = Integer.parseInt(etSegundos.text.toString())
+        val intent = Intent(this,AlarmeReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0)
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ (tempo * 1000), pendingIntent )
+
+    }
+}
